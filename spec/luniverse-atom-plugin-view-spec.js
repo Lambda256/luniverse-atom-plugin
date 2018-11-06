38039f3903b5e55b965f6b4d5a8c39f286d327f5
saltylight <mint@dunamu.com>
2018-11-06 12:48:31 +0900

    first commit

---
+++ spec/luniverse-atom-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LuniverseAtomPluginView from '../lib/luniverse-atom-plugin-view';
+
+describe('LuniverseAtomPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
